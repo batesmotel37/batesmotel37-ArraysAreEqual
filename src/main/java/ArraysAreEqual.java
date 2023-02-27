1aed12e19bd1f262b1b988690d2556178f1e2386
Kenneth High <kennethhigh7@gmail.com>
2023-02-27 10:58:49 +0000

    commit Array Equals Comparison Lab

--- src/main/java/ArraysAreEqual.java
+++ src/main/java/ArraysAreEqual.java
@@ -15,6 +15,14 @@ public class ArraysAreEqual {
      * @return true if the values of a are equal to the values of b.
      */
     public boolean equal(int[] a, int[] b){
-        return false;
+        if(a.length != b.length) return false;
+        else{
+            for (int itor = 0; itor < a.length; itor++){
+                if(a[itor] != b[itor]){
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
